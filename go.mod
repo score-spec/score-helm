a144c4975f54e4220d5ae3876ec231d8e98ea5dd
Eugene Y <ghen@users.noreply.github.com>
2023-06-12 23:03:47 +1000

    Overrides via command line parameters (#21)
    Signed-off-by: Eugene Yarshevich <yarshevich@gmail.com>

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/score-spec/score-go v0.0.0-20221019054335-3510902b5f8b
 	github.com/spf13/cobra v1.6.0
 	github.com/stretchr/testify v1.8.0
+	github.com/tidwall/sjson v1.2.5
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -17,5 +18,8 @@ require (
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tidwall/gjson v1.14.2 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- internal/command/run.go
+++ internal/command/run.go
@@ -8,18 +8,22 @@ The Apache Software Foundation (http://www.apache.org/).
 package command
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/imdario/mergo"
 	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/cobra"
+	"github.com/tidwall/sjson"
+	"gopkg.in/yaml.v3"
+
 	loader "github.com/score-spec/score-go/loader"
 	score "github.com/score-spec/score-go/types"
 	helm "github.com/score-spec/score-helm/internal/helm"
-	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
 const (
@@ -33,6 +37,8 @@ var (
 	outFile       string
 	valuesFile    string
 
+	overrideParams []string
+
 	verbose bool
 )
 
@@ -41,6 +47,8 @@ func init() {
 	runCmd.Flags().StringVar(&overridesFile, "overrides", overridesFileDefault, "Overrides file")
 	runCmd.Flags().StringVarP(&valuesFile, "values", "", "", "Imported values file (in YAML format)")
 	runCmd.Flags().StringVarP(&outFile, "output", "o", "", "Output file")
+
+	runCmd.Flags().StringArrayVarP(&overrideParams, "property", "p", nil, "Overrides selected property value")
 
 	runCmd.Flags().BoolVar(&verbose, "verbose", false, "Enable diagnostic messages (written to STDERR)")
 
@@ -76,7 +84,7 @@ func run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	// Apply overrides (optional)
+	// Apply overrides from file (optional)
 	//
 	if overridesFile != "" {
 		log.Printf("Checking '%s'...\n", overridesFile)
@@ -93,6 +101,41 @@ func run(cmd *cobra.Command, args []string) error {
 			}
 		} else if !os.IsNotExist(err) || overridesFile != overridesFileDefault {
 			return err
+		}
+	}
+
+	// Apply overrides from command line (optional)
+	//
+	for _, pstr := range overrideParams {
+		log.Print("Applying SCORE properties overrides...\n")
+
+		jsonBytes, err := json.Marshal(srcMap)
+		if err != nil {
+			return fmt.Errorf("marshalling score spec: %w", err)
+		}
+
+		pmap := strings.SplitN(pstr, "=", 2)
+		if len(pmap) <= 1 {
+			var path = pmap[0]
+			log.Printf("removing '%s'", path)
+			if jsonBytes, err = sjson.DeleteBytes(jsonBytes, path); err != nil {
+				return fmt.Errorf("removing '%s': %w", path, err)
+			}
+		} else {
+			var path = pmap[0]
+			var val interface{}
+			if err := yaml.Unmarshal([]byte(pmap[1]), &val); err != nil {
+				val = pmap[1]
+			}
+
+			log.Printf("overriding '%s' = '%s'", path, val)
+			if jsonBytes, err = sjson.SetBytes(jsonBytes, path, val); err != nil {
+				return fmt.Errorf("overriding '%s': %w", path, err)
+			}
+		}
+
+		if err = json.Unmarshal(jsonBytes, &srcMap); err != nil {
+			return fmt.Errorf("unmarshalling score spec: %w", err)
 		}
 	}
 
