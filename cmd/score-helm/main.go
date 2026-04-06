ca913f75b074b6ab92dd95dc9b8acd834715c8a5
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2026-04-06 13:59:14 -0400

    main.go
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>

--- cmd/score-helm/main.go
+++ cmd/score-helm/main.go
@@ -1,10 +1,17 @@
-/*
-Apache Score
-Copyright 2020 The Apache Software Foundation
+// Copyright 2024 The Score Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-This product includes software developed at
-The Apache Software Foundation (http://www.apache.org/).
-*/
 package main
 
 import (
@@ -15,7 +22,6 @@ import (
 )
 
 func main() {
-
 	if err := command.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, "Error: "+err.Error())
 		os.Exit(1)
